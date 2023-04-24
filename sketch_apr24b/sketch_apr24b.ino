c50cfb7d2a1905937643bd4ae4441f62ab0b7195
jacobssss14 <paya.jacob@gmail.com>
2023-04-24 11:19:53 +0800

    emz

--- sketch_apr24b/sketch_apr24b.ino
+++ sketch_apr24b/sketch_apr24b.ino
@@ -1,9 +1,30 @@
+//Declaire Variables
+#include <millisDelay.h>
+millisDelay ledDel;
+bool state_brush_in;
+int swt;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  //Set Pinout Mode
+  pinMode(2, OUTPUT);
+  pinMode(4, INPUT);
+  pinMode(7, OUTPUT);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  swt = digitalRead(4);
+  Serial.println(swt);
+  delay(500);
+
+  if swt = 0 {
+      state_brush_in = true; 
+  } else {
+      state_brush_in = false;
+  }; 
+
+
 
 }
