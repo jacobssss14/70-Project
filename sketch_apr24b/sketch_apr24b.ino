293b01d40729d2f0d9d3198226d1d283d5b4ab63
jacobssss14 <paya.jacob@gmail.com>
2023-04-29 20:37:00 +0800

    Add Functionality and Prototype Code
    *Added timer functions
    *Prototype code features

--- sketch_apr24b/sketch_apr24b.ino
+++ sketch_apr24b/sketch_apr24b.ino
@@ -1,10 +1,15 @@
+#include "Timer.h"
 volatile byte state_brush_in;
 int swt;
 int led_red = 4 ;
 int led_green = 7;
 int buzz = 8;
 int ledState = LOW;
-unsigned long time;
+unsigned long time_in;
+unsigned long time_out;
+unsigned long time_diff;
+
+Timer timer(MILLIS);
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,19 +35,41 @@ void loop() {
 
   switch (swt) {
    case 0:
+    timer.start();
+    time_in=timer.read();
+      if (time_in < 8000) {
+        //code repeats to line 39
+      } else if (time_in >= 8000) {
+        time_in = 8000;
+        digitalWrite(led_green, LOW);
+        digitalWrite(led_red, HIGH);
+        digitalWrite(buzz, HIGH);
+        delay(1000);
+        digitalWrite(led_green, LOW);
+        digitalWrite(buzz, LOW);
+        delay(1000);
+        timer.stop();
+        //repeat line 40
+        // case 0 repeats until interrupt on pin 2 (brush_out)
+      }
+    
+    break;
+    //brush_out condition
+   case 1:
     digitalWrite(led_red, LOW);
     digitalWrite(buzz, LOW);
     digitalWrite(led_green, HIGH);
-    break;
-     
-   case 1:
-    digitalWrite(led_green, LOW);
-    digitalWrite(led_red, HIGH);
-    digitalWrite(buzz, HIGH);
-    delay(1000);
-    digitalWrite(led_green, LOW);
-    digitalWrite(buzz, LOW);
-    delay(1000);
+    timer.start();
+    time_out=timer.read();
+    if (time_out<3000) {
+      //execute code to blink red led and sound buzzer
+    } else if (time_out >= 3000){
+      //execute code to blink green led and sound buzzer
+      time_out=3000;
+      timer.stop();
+      //jump to line 64
+      //alarm continues to sound until interrupt on pin 2
+    }
     break;
   }
 
