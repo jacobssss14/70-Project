068c7ba41be330d2fbcaec3ba637093c15c23838
jacobssss14 <paya.jacob@gmail.com>
2023-05-08 05:34:28 +0800

    Add Functionality
    *Added timers

--- sketch_apr24b/sketch_apr24b.ino
+++ sketch_apr24b/sketch_apr24b.ino
@@ -1,15 +1,17 @@
-#include "Timer.h"
+#include "CountDown.h"
+
+CountDown CD(CountDown::SECONDS);
+
 volatile byte state_brush_in;
 int swt;
 int led_red = 4 ;
 int led_green = 7;
 int buzz = 8;
 int ledState = LOW;
-unsigned long time_in;
-unsigned long time_out;
-unsigned long time_diff;
-
-Timer timer(MILLIS);
+long time_in = 0;
+long time_out=0;
+long time_diff=0;
+bool brushState = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,48 +32,55 @@ void brush_in() {
 
 void loop() {
   swt=digitalRead(2); 
-  // put your main code here, to run repeatedly:
+  
   //swt 0 = brush is in, swt 1 = brush is out
-
-  switch (swt) {
+switch (swt) {
    case 0:
-    timer.start();
-    time_in=timer.read();
-      if (time_in < 8000) {
-        //code repeats to line 39
-      } else if (time_in >= 8000) {
-        time_in = 8000;
-        digitalWrite(led_green, LOW);
-        digitalWrite(led_red, HIGH);
-        digitalWrite(buzz, HIGH);
-        delay(1000);
-        digitalWrite(led_green, LOW);
-        digitalWrite(buzz, LOW);
-        delay(1000);
-        timer.stop();
-        //repeat line 40
-        // case 0 repeats until interrupt on pin 2 (brush_out)
-      }
-    
-    break;
-    //brush_out condition
-   case 1:
-    digitalWrite(led_red, LOW);
+      CD.start(3);
+     while (CD.remaining() > 0) {
+      	digitalWrite(led_red, LOW);
+      	digitalWrite(buzz, LOW);
+      	digitalWrite(led_red, LOW);
+      	digitalWrite(buzz, LOW);
+      	digitalWrite(led_green, HIGH);
+      	Serial.println("brush is in");
+      	Serial.println(CD.remaining());
+     }
+    CD.stop();
+    Serial.println("brush is in for too long");
+    digitalWrite(led_green, LOW);
+    digitalWrite(led_red, HIGH);
+    digitalWrite(buzz, HIGH);
+    delay(1000);
+    digitalWrite(led_green, LOW);
     digitalWrite(buzz, LOW);
-    digitalWrite(led_green, HIGH);
-    timer.start();
-    time_out=timer.read();
-    if (time_out<3000) {
-      //execute code to blink red led and sound buzzer
-    } else if (time_out >= 3000){
-      //execute code to blink green led and sound buzzer
-      time_out=3000;
-      timer.stop();
-      //jump to line 64
-      //alarm continues to sound until interrupt on pin 2
-    }
+    delay(1000);
     break;
-  }
+     
+   case 1:
+   CD.start(3);
+     while (CD.remaining() > 0) {
+      	digitalWrite(led_red, LOW);
+      	digitalWrite(buzz, LOW);
+      	digitalWrite(led_red, LOW);
+      	digitalWrite(buzz, LOW);
+      	digitalWrite(led_green, HIGH);
+        delay(1000);
+        digitalWrite(led_green, LOW);
+      	Serial.println("brushing teeth");
+      	Serial.println(CD.remaining());
+     }
+    CD.stop();
+   	Serial.println("brush is out for too long");
+    digitalWrite(led_green, LOW);
+    digitalWrite(led_red, HIGH);
+    digitalWrite(buzz, HIGH);
+    delay(1000);
+    digitalWrite(led_green, LOW);
+    digitalWrite(buzz, LOW);
+    delay(1000);
+    break;
+}
 
 
 
